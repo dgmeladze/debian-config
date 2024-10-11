e9d4e8d460d3682a87618fe4b686b687ac0184e2
dgmeladze <128248810+dgmeladze@users.noreply.github.com>
2024-10-11 16:55:45 +0400

    Update jetbrains-tools.sh

--- tools/jetbrains-tools.sh
+++ tools/jetbrains-tools.sh
@@ -1,8 +1,36 @@
-#!/usr/bin/env bash
-## jetbrains ##
-printf "\033[1;35m Jetbrains... \033[0m\n"
-curl -s https://s3.eu-central-1.amazonaws.com/jetbrains-ppa/0xA6E8698A.pub.asc | gpg --dearmor | sudo tee /usr/share/keyrings/jetbrains-ppa-archive-keyring.gpg > /dev/null
-echo "deb [signed-by=/usr/share/keyrings/jetbrains-ppa-archive-keyring.gpg] http://jetbrains-ppa.s3-website.eu-central-1.amazonaws.com any main" | sudo tee /etc/apt/sources.list.d/jetbrains-ppa.list > /dev/null
+#!/bin/bash
 
-sudo apt install phpstorm -y
-sudo apt install pycharm-community -y
+# Проверка наличия jq и установка, если отсутствует
+if ! command -v jq &> /dev/null; then
+    echo "jq не установлен. Устанавливаю..."
+    sudo apt-get update
+    sudo apt-get install -y jq
+fi
+
+# Функция для установки программы
+install_jetbrains() {
+    local app_name=$1
+    local download_url=$2
+    local temp_file=$(mktemp)
+
+    echo "Скачивание $app_name..."
+    wget -O "$temp_file" "$download_url"
+
+    echo "Установка $app_name..."
+    sudo tar -xzf "$temp_file" -C /opt
+
+    echo "Удаление архива..."
+    rm "$temp_file"
+
+    echo "$app_name установлен."
+}
+
+# Получение последней версии PhpStorm
+phpstorm_url="https://download.jetbrains.com/webide/PhpStorm-2024.2.3.tar.gz"
+
+# Получение последней версии PyCharm Community
+pycharm_url="https://download.jetbrains.com/python/pycharm-community-2024.2.3.tar.gz"
+
+# Установка
+install_jetbrains "PhpStorm" "$phpstorm_url"
+install_jetbrains "PyCharm Community" "$pycharm_url"
