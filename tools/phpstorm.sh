79dac9fb515bfa6776456cc31d6716599552213b
dgmeladze <128248810+dgmeladze@users.noreply.github.com>
2024-10-11 15:41:59 +0400

    Update phpstorm.sh

--- tools/phpstorm.sh
+++ tools/phpstorm.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-sudo apt install phpstorm -y
+## jetbrains ##
+printf "\033[1;35m Jetbrains... \033[0m\n"
+curl -s https://s3.eu-central-1.amazonaws.com/jetbrains-ppa/0xA6E8698A.pub.asc | gpg --dearmor | sudo tee /usr/share/keyrings/jetbrains-ppa-archive-keyring.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/jetbrains-ppa-archive-keyring.gpg] http://jetbrains-ppa.s3-website.eu-central-1.amazonaws.com any main" | sudo tee /etc/apt/sources.list.d/jetbrains-ppa.list > /dev/null
+sudo apt install phpstorm pycharm-community -y
