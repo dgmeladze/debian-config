a3292a635144cc187bea8f4394fe9fa8cfa989dc
dgmeladze <128248810+dgmeladze@users.noreply.github.com>
2025-08-10 23:45:10 +0300

    Update chrome.sh

--- additional/chrome.sh
+++ additional/chrome.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 printf "\033[1;35m Chrome... \033[0m\n"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb -y
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt install -f -y # Resolve any dependency issues
+rm google-chrome-stable_current_amd64.deb
